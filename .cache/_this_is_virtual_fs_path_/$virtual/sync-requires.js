2df50eac272e72901bb20206c15e3361af491b5f
Brian Francoeur <francoeurbj@gmail.com>
2022-01-11 18:24:42 -0500

    add gatsby and gatsby-cli

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,9 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Brian\\Desktop\\Gatsby Projects\\WebSapient Website\\WebSapientReact\\websapientreact\\.cache\\dev-404-page.js"))
+}
+
